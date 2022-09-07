ca7be8da0f8b77be59ee417e43ef5e2db2ea6bb4
Radek Lada <radoslaw.lada@student.uj.edu.pl>
2022-09-07 21:01:24 +0200

    final changes

--- apptesting/settings.py
+++ apptesting/settings.py
@@ -117,5 +117,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_URL = '/19_lada/apptesting/static/'
+#MEDIA_URL = '/19_lada/apptesting/media'
